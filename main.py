00a6db6d163f3421f6d69be2be80ac2f01de96b7
newix1 <123808307+newix1@users.noreply.github.com>
2023-11-27 17:22:30 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import telebot
+from datetime import date
+
+from telebot import types, TeleBot
+
+bot: TeleBot = telebot.TeleBot('6773401761:AAGX38wdkr56sgE86MStW5xBeObs_boxMxM')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    current_date = date.today()
+    current_weekday = date.today().weekday()
+    weekday_dict = {0: 'Понедельник',
+                    1: 'Вторник',
+                    2: 'Среда',
+                    3: 'Четверг',
+                    4: 'Пятница',
+                    5: 'Суббота',
+                    6: 'Воскресенье'}
+    bot.send_message(message.chat.id, f'Дата сегодня: {current_date}\nДень недели: {weekday_dict[current_weekday]}',
+                     parse_mode='html')
+
+
+bot.infinity_polling()
